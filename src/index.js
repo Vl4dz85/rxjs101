1cda2ec13f7939c69da969117fe8d01c4e13097e
Vl4dz85 <Vl4dz85@gmail.com>
2022-06-08 14:43:54 +0400

    add clock exercise

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import "./interval";
+import "./clock";
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("title").innerHTML = "";
+  document.getElementById("title").innerHTML = "Building the Clock";
 });
